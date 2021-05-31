e414eafaf50bda72c591c551a9997588f24a9598
Skyler Harris <skyler.iqeq@gmail.com>
2021-05-31 12:31:30 +0800

    attempt for loop

--- looping_a_triangle/looping_a_triangle.js
+++ looping_a_triangle/looping_a_triangle.js
@@ -1,7 +1,12 @@
 
-// While Loop
-let hash = '#'
-while (hash.length <=7) {
-    console.log('#');
-    hash = hash + '#';
+// // While Loop
+// let hash = '#'
+// while (hash.length <=7) {
+//     console.log('#');
+//     hash = hash + '#';
+// }
+
+// For Loop
+for (let hash = '#'; hash.length <= 7; hash = hash + '#') {
+    console.log(hash);
 }
