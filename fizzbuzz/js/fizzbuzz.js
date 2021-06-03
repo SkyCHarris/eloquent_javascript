63e46c1eee370a91a29863122826bcb4bbefb479
Skyler Harris <skyler.iqeq@gmail.com>
2021-06-03 17:05:26 +0800

    for loop for fizz and buzz

---
+++ fizzbuzz/js/fizzbuzz.js
@@ -0,0 +1,12 @@
+
+for (let number = 0; number <=100; number = number + 1) {
+    if (number % 3 === 0) {
+        console.log('fizz');
+    }
+    else if (number % 5 === 0) {
+        console.log('buzz')
+    }
+    else {
+        console.log(number);
+    }
+}
